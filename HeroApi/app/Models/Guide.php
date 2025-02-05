1ab9190245f741c1f75ccf72c10d5dcb2643405d
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2025-02-05 13:48:43 +0100

    init models

--- HeroApi/app/Models/Guide.php
+++ HeroApi/app/Models/Guide.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Guide extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'guide';
+    protected $fillable = ['Weakness','Strength','Dengerousness','DestroyingPlan'];
 }
