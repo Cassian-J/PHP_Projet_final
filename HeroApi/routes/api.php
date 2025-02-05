a19a85ac3a94d413fdc46d3e421ef76e901fe4b9
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2025-02-06 00:20:01 +0100

    route for Super_Hero update

--- HeroApi/routes/api.php
+++ HeroApi/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\Super_HeroController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UsersController::class);
+Route::apiResource('super_hero', Super_HeroController::class);
