69bed496cf64385cda54da0a7c5e15cbab1eedc3
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2025-02-24 13:26:59 +0100

    superHero

--- My-Hero-App/public/javascripts/create_hero.js
+++ My-Hero-App/public/javascripts/create_hero.js
@@ -1,3 +1,7 @@
+import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
+
+window.socket = io();
+
 document.getElementById("createHero").onclick = function() {
     const heroInfo = {
         SuperHeroName: document.getElementById("superHeroName").value,
