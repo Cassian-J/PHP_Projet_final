6f86d9675d1d4d477032d72f5944b9ef8b173cc2
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2025-02-24 06:33:04 +0100

    modif route

--- My-Hero-App/routes/create_hero.js
+++ My-Hero-App/routes/create_hero.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-routter.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('create_hero', { title: 'Create Hero' });
 });
 
